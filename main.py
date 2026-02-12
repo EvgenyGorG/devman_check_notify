6f24f5840c3f9490a526069732062eb955cfe4be
Evgeny <evgenygorokhovg@yahoo.com>
2026-02-12 11:16:39 +0300

    Implement long polling for Devman API

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from pprint import pprint
+import logging
+from time import sleep
 
 import requests
 from environs import Env
@@ -6,17 +7,48 @@ from environs import Env
 env = Env()
 env.read_env()
 
+logging.basicConfig(
+    level=env.str('LOG_LEVEL', 'INFO'),
+    format='%(asctime)s [%(name)s] %(levelname)s %(message)s'
+)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
 
 def main():
-    auth_token = env.str("DEVMAN_API_TOKEN")
+    auth_token = env.str('DEVMAN_API_TOKEN')
 
-    url = 'https://dvmn.org/api/user_reviews/'
+    url = 'https://dvmn.org/api/long_polling/'
     headers = {'Authorization': f'Token {auth_token}'}
+    payload = {}
 
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
+    while True:
+        try:
+            response = requests.get(url, headers=headers, params=payload, timeout=100)
+            response.raise_for_status()
+            response_data = response.json()
 
-    pprint(response.json())
+            logger.debug(f'Ответ от сервера: {response_data}')
+
+            if response_data['status'] == 'found':
+                payload['timestamp'] = response_data['last_attempt_timestamp']
+            else:
+                payload['timestamp'] = response_data['timestamp_to_request']
+
+        except (
+                requests.exceptions.ConnectionError,
+                requests.exceptions.Timeout
+        ):
+            logger.warning(
+                'Ошибка соединения, подожду немного и попробую переподключиться...'
+            )
+            sleep(10)
+            continue
+
+        except requests.exceptions.HTTPError as error:
+            logger.exception(f'Ошибка HTTP: {error}')
+            break
 
 
 if __name__ == '__main__':
