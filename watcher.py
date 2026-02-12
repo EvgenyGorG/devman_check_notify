ded320f7097384e9a83c7de56cf0f5e318c88bc0
Evgeny <evgenygorokhovg@yahoo.com>
2026-02-12 14:22:22 +0300

    Connect Telegram bot for Devman review notifications

---
+++ watcher.py
@@ -0,0 +1,84 @@
+import logging
+from time import sleep
+
+import requests
+import telegram
+from environs import Env
+
+env = Env()
+env.read_env()
+
+logging.basicConfig(
+    level=env.str('LOG_LEVEL', 'INFO'),
+    format='%(asctime)s [%(name)s] %(levelname)s %(message)s'
+)
+logging.getLogger('urllib3').setLevel(logging.WARNING)
+logging.getLogger('telegram').setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    tg_token = env.str('TG_TOKEN')
+    chat_id = env.str('TG_CHAT_ID')
+    bot = telegram.Bot(token=tg_token)
+
+    auth_token = env.str('DEVMAN_API_TOKEN')
+
+    url = 'https://dvmn.org/api/long_polling/'
+    headers = {'Authorization': f'Token {auth_token}'}
+    payload = {}
+
+    while True:
+        try:
+            response = requests.get(url, headers=headers, params=payload, timeout=100)
+            response.raise_for_status()
+            response_data = response.json()
+
+            logger.debug(f'Ответ от сервера: {response_data}')
+
+            if response_data['status'] == 'timeout':
+                payload['timestamp'] = response_data['timestamp_to_request']
+            else:
+                bot.sendMessage(
+                    text='Работа проверена!',
+                    chat_id=chat_id
+                )
+                payload['timestamp'] = response_data['last_attempt_timestamp']
+
+        except (
+            requests.exceptions.ConnectionError,
+            requests.exceptions.Timeout,
+            telegram.error.NetworkError
+        ):
+            logger.warning(
+                'Ошибка соединения, подожду немного и попробую переподключиться...'
+            )
+            sleep(5)
+            continue
+
+        except requests.exceptions.HTTPError:
+            logger.exception(f'Ошибка HTTP.')
+            break
+
+        except telegram.error.Forbidden:
+            logger.exception(
+                f'Возможно бота заблокировали, заканчиваю работу.'
+            )
+            break
+
+        except telegram.error.BadRequest:
+            logger.exception(f'Ошибка.')
+            break
+
+        except telegram.error.TelegramError:
+            logger.exception(
+                f'Упс... Серьезная проблема с телеграмм.'
+                f'Попробую еще раз через время..'
+            )
+            sleep(60)
+            continue
+
+
+if __name__ == '__main__':
+    main()
