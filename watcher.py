1b02140dc094eb8ac1bb86c4cb41d5761c6e8f20
Evgeny <evgenygorokhovg@yahoo.com>
2026-02-15 18:05:00 +0300

    Style: improve code style

--- watcher.py
+++ watcher.py
@@ -11,14 +11,14 @@ logger = logging.getLogger(__name__)
 
 
 def main():
-    tg_token = env.str('TG_TOKEN')
-    chat_id = env.str('TG_CHAT_ID')
+    tg_token = env.str("TG_TOKEN")
+    chat_id = env.str("TG_CHAT_ID")
     bot = telegram.Bot(token=tg_token)
 
-    auth_token = env.str('DEVMAN_API_TOKEN')
+    auth_token = env.str("DEVMAN_API_TOKEN")
 
-    url = 'https://dvmn.org/api/long_polling/'
-    headers = {'Authorization': f'Token {auth_token}'}
+    url = "https://dvmn.org/api/long_polling/"
+    headers = {"Authorization": f"Token {auth_token}"}
     payload = {}
 
     while True:
@@ -27,78 +27,74 @@ def main():
             response.raise_for_status()
             response_data = response.json()
 
-            logger.debug(f'Ответ от сервера: {response_data}')
+            logger.debug(f"Ответ от сервера: {response_data}")
 
-            if response_data['status'] == 'timeout':
-                payload['timestamp'] = response_data['timestamp_to_request']
+            if response_data["status"] == "timeout":
+                payload["timestamp"] = response_data["timestamp_to_request"]
             else:
-                for attempt in response_data['new_attempts']:
-                    lesson_title = attempt['lesson_title']
-                    verdict =attempt['is_negative']
-                    lesson_url = attempt['lesson_url']
+                for attempt in response_data["new_attempts"]:
+                    lesson_title = attempt["lesson_title"]
+                    verdict = attempt["is_negative"]
+                    lesson_url = attempt["lesson_url"]
 
                     if verdict:
-                        verdict_message='К сожалению, в работе нашлись ошибки.'
+                        verdict_message = "К сожалению, в работе нашлись ошибки."
                     else:
-                        verdict_message=(
-                            'Преподавателю все понравилось,'
-                            ' можно приступать к следующему уроку.'
+                        verdict_message = (
+                            "Преподавателю все понравилось,"
+                            " можно приступать к следующему уроку."
                         )
 
                     bot.sendMessage(
                         text=(
                             f'У вас проверили работу "{lesson_title}".\n'
-                            f'Ссылка: {lesson_url}\n'
-                            f'{verdict_message}'
+                            f"Ссылка: {lesson_url}\n"
+                            f"{verdict_message}"
                         ),
-                        chat_id=chat_id
+                        chat_id=chat_id,
                     )
 
-                payload['timestamp'] = response_data['last_attempt_timestamp']
+                payload["timestamp"] = response_data["last_attempt_timestamp"]
 
         except (
             requests.exceptions.ConnectionError,
             requests.exceptions.Timeout,
-            telegram.error.NetworkError
+            telegram.error.NetworkError,
         ):
             logger.warning(
-                'Ошибка соединения,'
-                ' подожду немного и попробую переподключиться...'
+                "Ошибка соединения, подожду немного и попробую переподключиться..."
             )
             sleep(5)
             continue
 
         except requests.exceptions.HTTPError:
-            logger.exception(f'Ошибка HTTP.')
+            logger.exception("Ошибка HTTP.")
             break
 
         except telegram.error.Forbidden:
-            logger.exception(
-                f'Возможно бота заблокировали, заканчиваю работу.'
-            )
+            logger.exception("Возможно бота заблокировали, заканчиваю работу.")
             break
 
         except telegram.error.BadRequest:
-            logger.exception(f'Ошибка.')
+            logger.exception("Ошибка.")
             break
 
         except telegram.error.TelegramError:
             logger.exception(
-                f'Упс... Серьезная проблема с телеграмм.\n'
-                f'Попробую еще раз через время..'
+                "Упс... Серьезная проблема с телеграмм.\nПопробую еще раз через время.."
             )
             sleep(60)
             continue
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     env.read_env()
 
     logging.basicConfig(
-        level=env.str('LOG_LEVEL', 'INFO'),
-        format='%(asctime)s [%(name)s] %(levelname)s %(message)s'
+        level=env.str("LOG_LEVEL", "INFO"),
+        format="%(asctime)s [%(name)s] %(levelname)s %(message)s",
     )
-    logging.getLogger('urllib3').setLevel(logging.WARNING)
-    logging.getLogger('telegram').setLevel(logging.WARNING)
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+    logging.getLogger("telegram").setLevel(logging.WARNING)
 
     main()
